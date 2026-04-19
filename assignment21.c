9f59e59602b1de973dd7d249419ca83c76bdf8e5
anushkajain-hub <anushkajainn81@gmail.com>
2026-04-19 13:00:43 +0000

    assignments

---
+++ assignment21.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *source, *destination;
+    char ch;
+
+    
+    source = fopen("source.txt", "r");
+
+    
+    if (source == NULL) {
+        printf("Error: Cannot open source file.\n");
+        return 1;
+    }
+
+   
+    destination = fopen("destination.txt", "w");
+
+   
+    if (destination == NULL) {
+        printf("Error: Cannot create destination file.\n");
+        fclose(source);
+        return 1;
+    }
+
+    
+    while ((ch = fgetc(source)) != EOF) {
+        fputc(ch, destination);
+    }
+
+    printf("File copied successfully.\n");
+
+    
+    fclose(source);
+    fclose(destination);
+
+    return 0;
+}
